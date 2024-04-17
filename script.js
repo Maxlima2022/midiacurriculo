b98f7a7318183183cb5037a57a3679b8877087db
Maxlima2022 <137231151+Maxlima2022@users.noreply.github.com>
2024-04-17 18:53:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Dados para o gráfico de porcentagem de idiomas (exemplo)
+const languageData = {
+    labels: ['Inglês', 'Espanhol', 'Italiano',],
+    datasets: [{
+        label: 'Porcentagem de Idiomas',
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1,
+        data: [1, 25, 15,] // Porcentagens dos idiomas
+    }]
+};
+
+// Configurações do gráfico de porcentagem de idiomas
+const languageConfig = {
+    type: 'pie',
+    data: languageData,
+};
+
+// Criar o gráfico de porcentagem de idiomas
+var languageChart = new Chart(
+    document.getElementById('languageChart'),
+    languageConfig
+);
